0e94bdbe7b5ec1928c5bcff079a964c1a6e191f5
Trevor Harmon <tharmon@laspositascollege.edu>
2023-12-03 18:01:23 -0800

    Add solution

--- src/transitive_closure.h
+++ src/transitive_closure.h
@@ -5,5 +5,19 @@
 template <typename T, int N>
 void applyTransitiveClosure(Graph<T,N>& graph)
 {
-    // TODO
+    for (int k = 0; k < N; k++) {
+        for (int i = 0; i < N; i++) {
+            if (i != k) {
+                for (int j = 0; j < N; j++) {
+                    if (j != i && j != k) {
+                        if (graph.areAdjacent(i, k) &&
+                            graph.areAdjacent(k, j) &&
+                           !graph.areAdjacent(i, j)) {
+                            graph.insertDirectedEdge(i, j);
+                        }
+                    }
+                }
+            }
+        }
+    }
 }
